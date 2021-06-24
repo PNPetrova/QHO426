76a5631a0434abb81be7a0ae927250e088cb56ad
PNPetrova <>
2021-06-24 12:15:34 +0000

    added message

---
+++ basics/output/multiline_message.py
@@ -0,0 +1,9 @@
+# Displays message to standard output in multiline lines
+print("System Failiure Iminent!")
+print("System reboot has been initiated")
+print()
+print("...rebooting sensory system")
+print("...rebooting system motors")
+print("...rebooting hover engine")
+print()
+print("System Online")
