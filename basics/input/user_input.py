aff44c6300f53addd671e534ff82290853a7c642
PNPetrova <>
2021-06-30 18:49:15 +0000

    Added code to read user input

---
+++ basics/input/user_input.py
@@ -0,0 +1,4 @@
+# Ask user to enter their name
+print("What is your name human?")
+Petya = input()
+print("It is noce to meet you human" , Petya)
---
+++ basics/output/ascii_art.py
@@ -0,0 +1,5 @@
+# Display a Box 
+print("##########")
+print("#        #")
+print("#        #")
+print("##########")
